1fbf38c281827e4c1d0f7abb2ccdaa85a59c5615
timothyhipp <timothy.hipp@duke.edu>
2016-10-03 12:55:21 -0400

    Adds a silly comment

--- TurtleTracker.py
+++ TurtleTracker.py
@@ -4,6 +4,7 @@
 #
 # Created by: Tim Hipp (teh26@duke.edu)
 # Created on: Oct 2016
+# This is a new branch
 
 # Copy and paste a line of data as the startLine variable value
 lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
